bace6f2f962519578c4b45b72ab2980646f5e46e
tedraykov <tedraykov@gmail.com>
2019-08-18 15:25:07 +0300

    Summary section style and responsivness

--- src/app/home/summary/summary.component.ts
+++ src/app/home/summary/summary.component.ts
@@ -6,24 +6,24 @@ const staticArticles: Article[] = [
     title: 'Вътрешно обзавеждане',
     text: 'Вашето жилище е вашата крепост. Доверете ни се и ние ще Ви помогнем да го превърнете в уютния дом, за който мечтаете.',
     iconClass: 'icon-furnishing',
-    imageUrl: 'furnishing-thumbnail.jpg'
+    imageUrl: 'assets/furnishing-thumbnail.jpg'
   },
   {
     title: 'Модерен дизайн',
     text: 'Независимо от Вашите предпочитания за дизайн и оформление на вашия дом, нашите дизайнери ще вложат всичко необходимо за впечатляващ резултат.',
     iconClass: 'icon-modern-design',
-    imageUrl: 'modern-design-thumbnail.jpg'
+    imageUrl: 'assets/modern-design-thumbnail.jpg'
   },
   {
     title: 'Бизнес обзавеждане',
     text: 'Магазин, офис, хотел - с нас ще получите комплексна услуга от проектиране през изработка и монтаж на поръчаното от Вас обзавеждане.',
     iconClass: 'icon-business-design',
-    imageUrl: 'business-design-thumbnail.jpg'
+    imageUrl: 'assets/business-design-thumbnail.jpg'
   }, {
     title: 'Интериорен дизайн',
     text: 'От рисунка с калем на пясък до ползване на най-модерен софтуер, Злати е Вашият човек!',
     iconClass: 'icon-interior-design',
-    imageUrl: 'interior-design-thumbnail.jpg'
+    imageUrl: 'assets/interior-design-thumbnail.jpg'
   }
 ];
 
